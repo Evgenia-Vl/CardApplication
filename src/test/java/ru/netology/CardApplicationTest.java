bb37783682c8dca0a4c23557477ce2c768d9260a
EvgeniaVL <e9320915@yandex.ru>
2023-05-29 13:11:45 +0300

    Change1 CardApplicationTest

--- src/test/java/ru/netology/CardApplicationTest.java
+++ src/test/java/ru/netology/CardApplicationTest.java
@@ -20,4 +20,109 @@ public class CardApplicationTest {
 
     }
 
+    @Test
+    void fieldNameShouldNotContainLatinSymbols(){
+        SelenideElement form = $(".form");
+        open("http://localhost:9999/");
+        form.$("[data-test-id=name] input").setValue("Moshina Ksenia");
+        form.$("[data-test-id=phone] input").setValue("+79544568754");
+        form.$("[data-test-id=agreement]").click();
+        form.$("button").click();
+        $("[data-test-id=name].input_invalid .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+
+    }
+
+    @Test
+    void fieldNameShouldNotContainOnlySurname(){
+        SelenideElement form = $(".form");
+        open("http://localhost:9999/");
+        form.$("[data-test-id=name] input").setValue("Мошина");
+        form.$("[data-test-id=phone] input").setValue("+79544568754");
+        form.$("[data-test-id=agreement]").click();
+        form.$("button").click();
+        $("[data-test-id=name].input_invalid .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+
+    }
+
+    @Test
+    void fieldNameShouldNotContainOnlyName(){
+        SelenideElement form = $(".form");
+        open("http://localhost:9999/");
+        form.$("[data-test-id=name] input").setValue("Ксения");
+        form.$("[data-test-id=phone] input").setValue("+79544568754");
+        form.$("[data-test-id=agreement]").click();
+        form.$("button").click();
+        $("[data-test-id=name].input_invalid .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+
+    }
+
+    @Test
+    void shouldBeMessageAboutMistakeIfFieldNameIsNotCompleted(){
+        SelenideElement form = $(".form");
+        open("http://localhost:9999/");
+        form.$("[data-test-id=name] input").setValue("");
+        form.$("[data-test-id=phone] input").setValue("+79544568754");
+        form.$("[data-test-id=agreement]").click();
+        form.$("button").click();
+        $("[data-test-id=name].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+
+    }
+    @Test
+    void fieldNameCanContainSymbolYo(){
+        SelenideElement form = $(".form");
+        open("http://localhost:9999/");
+        form.$("[data-test-id=name] input").setValue("Мухин Пётр");
+        form.$("[data-test-id=phone] input").setValue("+79544568754");
+        form.$("[data-test-id=agreement]").click();
+        form.$("button").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+
+    }
+    @Test
+    void fieldNameShouldNotContainSpecialCharacters(){
+        SelenideElement form = $(".form");
+        open("http://localhost:9999/");
+        form.$("[data-test-id=name] input").setValue("@Мошина Ксения");
+        form.$("[data-test-id=phone] input").setValue("+79544568754");
+        form.$("[data-test-id=agreement]").click();
+        form.$("button").click();
+        $("[data-test-id=name].input_invalid .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+
+    }
+
+    @Test
+    void shouldBeMessageAboutMistakeIfItHaveNotPlusBeforeNumber(){
+        SelenideElement form = $(".form");
+        open("http://localhost:9999/");
+        form.$("[data-test-id=name] input").setValue("Мошина Ксения");
+        form.$("[data-test-id=phone] input").setValue("79544568754");
+        form.$("[data-test-id=agreement]").click();
+        form.$("button").click();
+        $("[data-test-id=phone].input_invalid .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+
+    }
+
+        @Test
+    void shouldBeMessageAboutMistakeIfNumberHaveDashes(){
+        SelenideElement form = $(".form");
+        open("http://localhost:9999/");
+        form.$("[data-test-id=name] input").setValue("Мошина Ксения");
+        form.$("[data-test-id=phone] input").setValue("+7-954-456-87-54");
+        form.$("[data-test-id=agreement]").click();
+        form.$("button").click();
+            $("[data-test-id=phone].input_invalid .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+
+        }
+
+    @Test
+    void shouldBeMessageAboutMistakeIfFieldNumberIsNotCompleted(){
+        SelenideElement form = $(".form");
+        open("http://localhost:9999/");
+        form.$("[data-test-id=name] input").setValue("Мошина Ксения");
+        form.$("[data-test-id=phone] input").setValue("");
+        form.$("[data-test-id=agreement]").click();
+        form.$("button").click();
+        $("[data-test-id=phone].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+
+    }
 }
